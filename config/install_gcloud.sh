4c74d9b75dd248910d4529612442d84f231457d1
Ryan Collins <rlcollins@g.harvard.edu>
2022-06-30 15:48:40 -0400

    Update Google SDK install script for Ubuntu

--- config/install_gcloud.sh
+++ config/install_gcloud.sh
@@ -1,20 +1,17 @@
 #!/usr/bin/env bash
 
-# Install Google Cloud SDK
+# Install Google Cloud SDK on Ubuntu
 
 apt-get -qqy clean && \
 apt-get -qqy update --fix-missing && \
 apt-get -qqy dist-upgrade && \
-apt-get -qqy install lsb-release && \
-pip install -q -U --no-cache-dir \
-            crcmod \
-            wheel && \
-export CLOUD_SDK_REPO="cloud-sdk-$( lsb_release -c -s )" && \
-echo "deb https://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" > /etc/apt/sources.list.d/google-cloud-sdk.list && \
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - && \
+apt-get -qqy install lsb-release apt-transport-https ca-certificates gnupg && \
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" \
+| tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && \
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | \
+apt-key --keyring /usr/share/keyrings/cloud.google.gpg add - && \
 apt-get -qqy update && \
-apt-get -qqy install --no-install-recommends \
-             google-cloud-sdk && \
+apt-get -qqy install --no-install-recommends google-cloud-sdk && \
 gcloud config set core/disable_usage_reporting true && \
 gcloud config set component_manager/disable_update_check true && \
 gcloud config set metrics/environment github_docker_image && \
